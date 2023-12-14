ece4d9b6d00be0f90101f0b41d5fe196b5686005
MEscape <123681715+MEscape@users.noreply.github.com>
2023-12-14 21:45:57 +0100

    Update keyExchange.js

--- keyExchange.js
+++ keyExchange.js
@@ -9,6 +9,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    next()
+})
+
 let sessions = new Set()
 let finished = new Set()
 let playerCount = 0
